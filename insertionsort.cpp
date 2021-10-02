b2c9b459a196911b304b451a49dd9372f599728d
AkashMittal-30 <78265224+AkashMittal-30@users.noreply.github.com>
2021-10-02 15:02:51 +0530

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -13,14 +13,14 @@ int main()
     int i, key, j;
     for (i = 1; i < n; i++)
     {
-        key = arr[i];
+        key = arr[i]; //storing the latest unsorted number
         j = i - 1;
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 && arr[j] > key) 
         {
             arr[j + 1] = arr[j];
             j = j - 1;
-        }
-        arr[j + 1] = key;
+        } //locating position towards the right of unsorted number(sorted part) to see where it fits
+        arr[j + 1] = key; //putting the unsorted number in the correct position in the sorted part
     }
     for (i = 0; i < n; i++){cout<<arr[i]<< " ";}
     cout<<"\n";
